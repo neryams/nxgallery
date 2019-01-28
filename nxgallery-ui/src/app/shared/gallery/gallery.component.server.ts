868fd227cbafe2715b1a52d014957fa3b7737204
Raymon Ohmori <neryams@gmail.com>
2019-01-28 01:52:28 -0500

    fix(client): fix prod build trying to load removed scss file

--- nxgallery-ui/src/app/shared/gallery/gallery.component.server.ts
+++ nxgallery-ui/src/app/shared/gallery/gallery.component.server.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, DoCheck, ElementRef, EventEmitter, Input, Output, ViewChild  } from '@angular/core';
+import { AfterViewInit, Component, DoCheck, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 import { IImageDocument } from './../../../../../shared/interfaces/imageData';
 
@@ -9,21 +9,21 @@ export interface LoadingImage {
 }
 
 export interface ImagePosition {
-  _id: string,
-  position: { x: number, y: number }
+  _id: string;
+  position: { x: number; y: number };
 }
 
 export interface GalleryItem {
-  url?: string,
-  id: string,
-  aspect: number,
-  newItem: boolean
+  url?: string;
+  id: string;
+  aspect: number;
+  newItem: boolean;
 }
 
 @Component({
   selector: 'nxg-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['../../../assets/sass/gallery.scss', './gallery.component.scss']
+  styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements AfterViewInit, DoCheck {
   @Input() images: Array<IImageDocument>;
@@ -46,21 +46,25 @@ export class GalleryComponent implements AfterViewInit, DoCheck {
   }
 
   ngDoCheck(): void {
-    if(this.imagesLength !== this.images.length || this.progressLength !== this.progress.length) {
+    if (this.imagesLength !== this.images.length || this.progressLength !== this.progress.length) {
       this.currentImages = this.images.map((imageData: IImageDocument) => ({
         url: imageData.imageUrls['600'],
         id: imageData._id,
         aspect: imageData.info.aspect,
         newItem: false
       }));
-  
-      if(this.progress) {
+
+      if (this.progress) {
         const currentProgress: Array<LoadingImage> = this.progress;
-        this.currentImages.unshift(...currentProgress.map((imageData: LoadingImage) => ({
-          id: imageData.uid,
-          aspect: imageData.aspect,
-          newItem: true
-        })).reverse())
+        this.currentImages.unshift(
+          ...currentProgress
+            .map((imageData: LoadingImage) => ({
+              id: imageData.uid,
+              aspect: imageData.aspect,
+              newItem: true
+            }))
+            .reverse()
+        );
       }
     }
   }
@@ -70,9 +74,9 @@ export class GalleryComponent implements AfterViewInit, DoCheck {
       gutter: 10
     });
 
-    this.gridInst.on('layoutComplete', ( laidOutItems: Array<any> ) => {
+    this.gridInst.on('layoutComplete', (laidOutItems: Array<any>) => {
       let itemsChanged = false;
-      laidOutItems.forEach((item: { position: ImagePosition['position'], element: HTMLElement }) => {
+      laidOutItems.forEach((item: { position: ImagePosition['position']; element: HTMLElement }) => {
         this.updatedImagesCollection.set(item.element.id, {
           _id: item.element.id,
           position: { x: item.position.x, y: item.position.y } // Have to clone to prevent references updating on their own
@@ -85,17 +89,14 @@ export class GalleryComponent implements AfterViewInit, DoCheck {
           clearTimeout(this.outputDebounce);
         }
         this.outputDebounce = setTimeout(() => {
-          this.updatedImages.emit( Array.from(this.updatedImagesCollection.values()) );
+          this.updatedImages.emit(Array.from(this.updatedImagesCollection.values()));
           this.updatedImagesCollection.clear();
         }, 500);
       }
-    })
+    });
   }
-
 }
 
 class Packery {
-  constructor(private element: HTMLElement, options: any) {
-
-  }
+  constructor(private readonly element: HTMLElement, options: any) {}
 }
