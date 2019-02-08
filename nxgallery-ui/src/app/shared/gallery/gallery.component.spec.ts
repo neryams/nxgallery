afb1b9f3ab9ca3582dc4437483edd3b7ec0f2c1a
Raymon Ohmori <neryams@gmail.com>
2019-02-08 01:23:54 -0500

    fix(tests): fix gallery unit test

--- nxgallery-ui/src/app/shared/gallery/gallery.component.spec.ts
+++ nxgallery-ui/src/app/shared/gallery/gallery.component.spec.ts
@@ -1,4 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MockImageService } from '~/app/framework/images/testing/mocks/image-service.mock';
 import { t } from '~/app/framework/testing';
@@ -15,7 +21,15 @@ describe('GalleryComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [GalleryItemsDirective, GalleryComponent],
-      imports: [MatProgressBarModule]
+      imports: [
+        ReactiveFormsModule,
+        MatProgressBarModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule
+      ]
     }).compileComponents();
   }));
 
